8ef21d9168999f27cb8c6b8e3b56ffb1fb2dbf44
FGalerne <florian.galerne@free.fr>
2016-12-06 20:04:06 +0100

    mayor.html.twig and css

--- src/BoosterBundle/Controller/DefaultController.php
+++ src/BoosterBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function mayorAction()
     {
-        return $this->render('BoosterBundle:Default:index.html.twig');
+        return $this->render('BoosterBundle::mayor.html.twig');
     }
 }
