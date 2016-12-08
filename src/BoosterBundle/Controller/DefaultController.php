72182d9e56e4dbd80c9b5766fde6d74ab3ff2ba5
Julien <j.saintlaurent44@gmail.com>
2016-12-08 09:49:14 +0100

    first commit test

--- src/BoosterBundle/Controller/DefaultController.php
+++ src/BoosterBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function home_page_no_connectAction()
     {
-        return $this->render('BoosterBundle:Default:index.html.twig');
+        return $this->render('BoosterBundle::home_page_no_connect.html.twig');
     }
 }
