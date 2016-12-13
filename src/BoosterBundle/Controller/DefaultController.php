d799ae046302e725737e56a6c389f6998b51052e
PascaleB1 <barboteaupascale@yahoo.fr>
2016-12-13 17:03:41 +0100

    creatteblock identity

--- src/BoosterBundle/Controller/DefaultController.php
+++ src/BoosterBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BoosterBundle:Default:index.html.twig');
+        return $this->render('BoosterBundle::citizen.html.twig');
     }
 }
