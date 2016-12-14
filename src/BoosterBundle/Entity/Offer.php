15dd3883676c6d20db546f16a13d121e95faff82
FGalerne <florian.galerne@free.fr>
2016-12-14 17:41:53 +0100

    creation of entities

---
+++ src/BoosterBundle/Entity/Offer.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace BoosterBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Offer
+ */
+class Offer
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $cp;
+
+    /**
+     * @var string
+     */
+    private $town;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var int
+     */
+    private $lat;
+
+    /**
+     * @var int
+     */
+    private $lgt;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Offer
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set cp
+     *
+     * @param string $cp
+     * @return Offer
+     */
+    public function setCp($cp)
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+    /**
+     * Get cp
+     *
+     * @return string 
+     */
+    public function getCp()
+    {
+        return $this->cp;
+    }
+
+    /**
+     * Set town
+     *
+     * @param string $town
+     * @return Offer
+     */
+    public function setTown($town)
+    {
+        $this->town = $town;
+
+        return $this;
+    }
+
+    /**
+     * Get town
+     *
+     * @return string 
+     */
+    public function getTown()
+    {
+        return $this->town;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Offer
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set lat
+     *
+     * @param integer $lat
+     * @return Offer
+     */
+    public function setLat($lat)
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    /**
+     * Get lat
+     *
+     * @return integer 
+     */
+    public function getLat()
+    {
+        return $this->lat;
+    }
+
+    /**
+     * Set lgt
+     *
+     * @param integer $lgt
+     * @return Offer
+     */
+    public function setLgt($lgt)
+    {
+        $this->lgt = $lgt;
+
+        return $this;
+    }
+
+    /**
+     * Get lgt
+     *
+     * @return integer 
+     */
+    public function getLgt()
+    {
+        return $this->lgt;
+    }
+}
