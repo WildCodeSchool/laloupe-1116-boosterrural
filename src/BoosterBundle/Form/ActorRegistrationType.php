eb46246e2fb50c5d6fbd17e0fcbf32404df1a17b
Julien <j.saintlaurent44@gmail.com>
2016-12-31 15:00:14 +0100

    Last Commit formActor ALL IS OK

--- src/BoosterBundle/Form/ActorRegistrationType.php
+++ src/BoosterBundle/Form/ActorRegistrationType.php
@@ -2,6 +2,7 @@
 namespace BoosterBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 class ActorRegistrationType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -16,16 +17,26 @@ class ActorRegistrationType extends AbstractType {
             -> add('firstname', null, array(
                 'label'=>'prénom du responsable'
             ))
-            -> add('status_actor', null, array(
+            -> add('status_actor', ChoiceType::class, array(
+                'choices' => array(
+                    'Réseaux ' => 'Réseaux ',
+                    'Acteur individuel' => 'Acteur individuel',
+                    'TPE/Artisans' => 'TPE/Artisans'),
                 'label'=>'statut'
             ))
-            -> add('category', null, array(
+            -> add('category', ChoiceType::class, array(
+                'choices' => array(
+                    'Développement durable' => 'Développement durable',
+                    'Habitat' => 'Habitat',
+                    'Agriculture/Agroalimentaire' => 'Agriculture/Agroalimentaire',
+                    'Culture/Patrimoine' => 'Culture/Patrimoine',
+                    'Mobilité' => 'Mobilité'),
                 'label'=>'categorie'
             ))
             -> add('town', null, array(
                 'label'=>'nom de la commune'
             ))
-            -> add('cp', null, array(
+            -> add('cp', 'text', array(
                 'label'=>'code postal'
             ))
             -> add('address', null, array(
