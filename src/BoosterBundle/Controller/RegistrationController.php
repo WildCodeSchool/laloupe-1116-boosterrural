27d4646701d08dcd7cb2a805b6be5439d284e08b
PascaleB1 <barboteaupascale@yahoo.fr>
2017-01-04 15:33:30 +0100

    delete file check_mail and delete buttons modales

--- src/BoosterBundle/Controller/RegistrationController.php
+++ src/BoosterBundle/Controller/RegistrationController.php
@@ -144,4 +144,24 @@ class RegistrationController extends BaseController
             }
         }
     }
+
+    public function checkEmailAction()
+    {
+        $email = $this->get('session')->get('fos_user_send_confirmation_email/email');
+
+        if (empty($email)) {
+            return new RedirectResponse($this->get('router')->generate('fos_user_registration_register'));
+        }
+
+        $this->get('session')->remove('fos_user_send_confirmation_email/email');
+        $user = $this->get('fos_user.user_manager')->findUserByEmail($email);
+
+        if (null === $user) {
+            throw new NotFoundHttpException(sprintf('The user with email "%s" does not exist', $email));
+        }
+
+        return $this->render('BoosterBundle::home_page_no_connect.html.twig', array(
+            'user' => $user,
+        ));
+    }
 }
