fda984d0edd5fc2f17e7f1a1d029bd726573e71d
FGalerne <florian.galerne@free.fr>
2017-01-06 10:10:08 +0100

    chance label in form offer

--- src/BoosterBundle/Form/OfferType.php
+++ src/BoosterBundle/Form/OfferType.php
@@ -5,6 +5,7 @@ namespace BoosterBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class OfferType extends AbstractType
 {
@@ -13,9 +14,41 @@ class OfferType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title')->add('cp')->add('town')->add('description')->add('activity')->add('wish')->add('lat')->add('lgt')->add('users')        ;
+        $builder
+            ->add('title', null, array(
+                'label'=>'Titre de l\'offre'
+            ))
+            ->add('cp', null, array(
+                'label'=>'Code postal'
+            ))
+            ->add('town', null, array(
+                'label'=>'Ville'
+            ))
+            ->add('activity', choiceType::class, array(
+                'choices' => array(
+                    'hobby' => 'Loisirs',
+                    'craftsman'=> 'Artisan',
+                    'benevolat'=> 'Bénévolats',
+                    'communication'=>'Communication'
+                ),
+                'label'=>'Categorie'))
+            ->add('description', null, array(
+                'label'=>'Description'
+            ))
+
+
+            ->add('wish', choiceType::class, array(
+                'choices' => array(
+                    'dupliquate' => 'Dupliquer ce projet',
+                    'formation'=> 'Former sur ce projet',
+                    'help'=> 'Appuyer un projet similaire',
+                    'tell'=>'Echanger sur ce projet'
+                ),
+                'label'=>'Souhaits'))
+
+        ;
     }
-    
+
     /**
      * {@inheritdoc}
      */
