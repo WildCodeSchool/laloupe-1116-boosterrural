8417bdace83406d54cf3ca299a6a276db98fceed
PascaleB1 <barboteaupascale@yahoo.fr>
2017-01-10 10:38:49 +0100

    add namespace and use for NeedsForm

--- src/BoosterBundle/Form/NeedsType.php
+++ src/BoosterBundle/Form/NeedsType.php
@@ -1,51 +1,61 @@
-{
-/**
-* {@inheritdoc}
-*/
-public function buildForm(FormBuilderInterface $builder, array $options)
-{
-$builder->add('title', null, [
-"label" => "Titre"
-])
-->add('cp', null, [
-"label" => "Code postal"
-])
-->add('town', null, [
-"label" => "Ville"
-])
-->add('activity', ChoiceType::class, array (
-"label" => "Domaine d'activité",
-'choices'  => array(
-"Choisir" => "Choisir",
-"Loisirs" => "Loisirs",
-"Artisan" => "Artisan",
-"Bénévolat" => "Bénévolat",
-"Communication" => "Communication")))
-->add('description', null, [
-"label" => "Description"
-])
-->add('image2')
-->add('file2','file', array( 'required'=>false))
-;
-}
+<?php
 
-/**
-* {@inheritdoc}
-*/
-public function configureOptions(OptionsResolver $resolver)
-{
-$resolver->setDefaults(array(
-'data_class' => 'BoosterBundle\Entity\Needs'
-));
-}
+namespace BoosterBundle\Form;
 
-/**
-* {@inheritdoc}
-*/
-public function getBlockPrefix()
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class NeedsType extends AbstractType
 {
-return 'boosterbundle_needs';
-}
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title', null, [
+            "label" => "Titre"
+        ])
+            ->add('cp', null, [
+                "label" => "Code postal"
+            ])
+            ->add('town', null, [
+                "label" => "Ville"
+            ])
+            ->add('activity', ChoiceType::class, array (
+                "label" => "Domaine d'activité",
+                'choices'  => array(
+                    "Choisir" => "Choisir",
+                    "Loisirs" => "Loisirs",
+                    "Artisan" => "Artisan",
+                    "Bénévolat" => "Bénévolat",
+                    "Communication" => "Communication")))
+            ->add('description', null, [
+                "label" => "Description"
+            ])
+            ->add('image2')
+            ->add('file2','file', array( 'required'=>false))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BoosterBundle\Entity\Needs'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'boosterbundle_needs';
+    }
 
 
 }
