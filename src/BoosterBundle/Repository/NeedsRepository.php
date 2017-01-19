bbe01dcf63b98166e19c23004ea3b2b24a8bee09
PascaleB1 <barboteaupascale@yahoo.fr>
2017-01-19 11:52:05 +0100

    end of searchbar

--- src/BoosterBundle/Repository/NeedsRepository.php
+++ src/BoosterBundle/Repository/NeedsRepository.php
@@ -8,21 +8,5 @@ use Doctrine\ORM\EntityRepository;
 class NeedsRepository extends EntityRepository
 {
 
-   public function findNeedsBydescription($motcle)
-    {
-        $query = $this->createQueryBuilder('n')
-            ->where('n.description like :description')
-            ->orwhere('n.title like :title')
-            ->orwhere('n.activity like :activity')
-            ->orwhere('n.town like :town')
-            ->setParameter('description', '%'.$motcle.'%')
-            ->setParameter('title', '%'.$motcle.'%')
-            ->setParameter('activity', '%'.$motcle.'%')
-            ->setParameter('town', '%'.$motcle.'%')
-            ->orderBy('n.description', 'ASC')
-            ->getQuery();
 
-
-        return $query->getResult();
-    }
 }
