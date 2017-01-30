4a161f34aa10ff6a39adffd52f337cfb1292cc49
PascaleB1 <barboteaupascale@yahoo.fr>
2017-01-30 11:13:55 +0100

    custom button and redirection

--- src/BoosterBundle/Controller/ProfCitizenController.php
+++ src/BoosterBundle/Controller/ProfCitizenController.php
@@ -63,7 +63,7 @@ class ProfCitizenController extends Controller
             $em->persist($offer);
             $em->flush($offer);
 
-            return $this->redirectToRoute('citizen_showOffer', array('id' => $offer->getId(
+            return $this->redirectToRoute('citizen_index', array('id' => $offer->getId(
                 array($offer->getUsers()
                 ))));
 
@@ -91,7 +91,7 @@ class ProfCitizenController extends Controller
             $em->persist($needs);
             $em->flush($needs);
 
-            return $this->redirectToRoute('citizen_showNeeds', array('id' => $needs->getId(
+            return $this->redirectToRoute('citizen_index', array('id' => $needs->getId(
                 array($needs->getUsers()
                 ))));
 
@@ -177,7 +177,7 @@ class ProfCitizenController extends Controller
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('actor_index', array('id' => $user->getId()));
+            return $this->redirectToRoute('citizen_index', array('id' => $user->getId()));
         }
 
 
@@ -205,7 +205,7 @@ class ProfCitizenController extends Controller
             return $this->redirectToRoute('citizen_index', array('id' => $user->getId()));
         }
 
-        return $this->render('BoosterBundle:Citizen:editDescription.html.twig', array(
+        return $this->render('BoosterBundle:Citizen:editUser.html.twig', array(
             'user' => $user,
             'form' => $form->createView(),
 
